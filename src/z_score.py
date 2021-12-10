04c67300cd10b356d2e354d92bfe5af55f7d6387
Ilse Lagerwaard <i.m.lagerwaard@students.uu.nl>
2021-12-10 13:46:26 +0100

    Restore files

--- src/z_score.py
+++ src/z_score.py
@@ -1,1 +1,43 @@
+class ZScoreSelector:
+
+
+    def __init__(self):
+
+        from PyQt5.QtWidgets import QDialog, QLabel, QLineEdit, QPushButton, QDialogButtonBox, QHBoxLayout, QVBoxLayout 
+
+        self.main_dialog = QDialog()
+        self.main_dialog.setWindowTitle("Create HADDOCK input from DisVis output")
+
+        label = QLabel("Select a DisVis output folder:")
+        line_edit = QLineEdit()
+        file_button = QPushButton()
+        file_button.clicked.connect(self.file_dialog(line_edit))
+        ok_cancel = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        ok_cancel.accepted.connect(self.ok_clicked)
+        ok_cancel.rejected.connect(self.main_dialog.close)
+
+        file_layout = QHBoxLayout()
+        file_layout.addWidget(line_edit)
+        file_layout.addWidget(file_button)
+        
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(label)
+        main_layout.addLayout(file_layout)
+        main_layout.addWidget(ok_cancel)
+
+        self.main_dialog.setLayout(main_layout)
+        self.main_dialog.show()
+        
+
+    def file_dialog(self, line_edit):
+
+        from PyQt5.QtWidgets import QFileDialog
+
+        return
+
+
+    def ok_clicked(self):
+
+        return
+
 
